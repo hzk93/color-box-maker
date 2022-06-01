a49d20bc75c5e7befb22e47c7e41b11cad5c217c
hzk93 <hassanzk93@gmail.com>
2022-06-01 16:25:09 +0500

    learning react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
+// simport NewBoxForm from './NewBoxForm';
+import Box from './Box';
+import BoxList from './BoxList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  render(){
+    return(
+    <div>
+      <BoxList/>
+      <Box/>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
